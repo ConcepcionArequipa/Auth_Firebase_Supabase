928eab55b6cc87d86c3b194a60d0da7064f09948
Camila Bueno <camibuenorivera2005@gmail.com>
2026-05-06 21:47:31 -0500

    cambio

--- firebase-login-fullstack/backend/src/firebaseAdmin.js
+++ firebase-login-fullstack/backend/src/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json"); // Asegúrate de que el nombre coincida
+const serviceAccount = require("../serviceAccountKey.json"); 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
